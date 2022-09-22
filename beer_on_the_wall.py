7ecc0637c1eb9b5667f9e66ce2f97e9216cbd61b
techsagemc <techsagemc@gmail.com>
2022-09-22 15:13:00 -0400

    make beer_on_the_wall.py more efficient / concise

--- beer_on_the_wall.py
+++ beer_on_the_wall.py
@@ -1,19 +1,16 @@
 
 for num in range(99,0,-1):
-    if num >= 2:
+    if num != 1:
         print(f"{num} bottles of beer on the wall")
         print(f"{num} bottles of beer.")
-    if num == 2:
-        print(f"take one down, pass it around, {num - 1} bottle of beer on the wall.")
+        print(f"take one down, pass it around, {num - 1} bottles of beer on the wall.")
     
-    elif num == 1:
+    else:
         print(f"{num} bottle of beer on the wall")
         print(f"{num} bottle of beer.")
-        print(f"take one down, pass it around, no more bottles of beer on the wall.")
-        
-    else: 
-        print(f"take one down, pass it around, {num - 1} bottles of beer on the wall.")
+        print("take one down, pass it around, no more bottles of beer on the wall.")
     
     print("*" * 50)
     
     
+ 
